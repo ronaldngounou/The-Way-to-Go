6711fc4fb62655e5b6445c781853b6e4a72f5014
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-04-19 13:59:07 -0700

    Hello world completed

--- LearnGoWithTests/hello-world/hello.go
+++ LearnGoWithTests/hello-world/hello.go
@@ -1,12 +1,30 @@
 package main 
 
-const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(word string) string {
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(word string, language string) string {
 	if word == ""{
 		word = "World"
 	}
-	return englishHelloPrefix + word
+	
+	return greetingPrefix(language) + word
 }
 
+
+func greetingPrefix(language string) (prefix string){
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 
--- LearnGoWithTests/hello-world/hello_test.go
+++ LearnGoWithTests/hello-world/hello_test.go
@@ -6,15 +6,28 @@ func TestHello(t *testing.T) {
 	// The next requirement is when the function is called with an emoty string, it defaults to
 	// printing "Hello, World", rather than "Hello,"
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying Hello, World when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("John", "French")
+		want := "Bonjour, John"
+		assertCorrectMessage(t, got, want)
+
+	})
+
 
 }
 
