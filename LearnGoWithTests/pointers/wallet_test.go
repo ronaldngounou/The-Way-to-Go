7f5d7d386278c4b4be1caab0cf233ab00efb0add
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-04-21 22:20:55 -0700

    Refactor unit tests

--- LearnGoWithTests/pointers/wallet_test.go
+++ LearnGoWithTests/pointers/wallet_test.go
@@ -4,17 +4,20 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin){
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 	
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdrawal", func(t *testing.T) {
@@ -22,12 +25,7 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(20)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(20))
 
 	})
 
