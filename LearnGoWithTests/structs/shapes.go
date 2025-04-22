4c9c55d3c67398a36d409267842ca2cbf6263865
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-04-21 21:14:58 -0700

    Implement Rectangle struct

--- LearnGoWithTests/structs/shapes.go
+++ LearnGoWithTests/structs/shapes.go
@@ -1,9 +1,14 @@
 package main 
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectange struct {
+	Width float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func Perimeter(rectangle Rectange) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectange) float64 {
+	return rectangle.Width * rectangle.Height
 }
--- LearnGoWithTests/structs/shapes_test.go
+++ LearnGoWithTests/structs/shapes_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectange{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,8 +13,9 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
+	rectangle := Rectange{10.0, 10.0}
+	got := Area(rectangle)
+	want := 100.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
