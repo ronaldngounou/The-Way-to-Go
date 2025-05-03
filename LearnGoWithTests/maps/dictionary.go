144f09a482af8f272cd4ef2b97523ad5951714f3
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-05-03 13:42:10 -0700

    Learned dictionaries
    Signed-off-by: Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>

--- LearnGoWithTests/maps/dictionary.go
+++ LearnGoWithTests/maps/dictionary.go
@@ -1,7 +1,20 @@
 package main
 
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, val string) {
+	d[key] = val
 }
--- LearnGoWithTests/maps/dictionary_test.go
+++ LearnGoWithTests/maps/dictionary_test.go
@@ -5,16 +5,55 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertErrror(t, got, ErrNotFound)
+	})
+
+	
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("Marie", "1234")
+
+	want := "0000"
+	got, err := dictionary.Search("Marie")
+
+	if err != nil {
+		t.Fatal("Unable to find the word. Error happened: ", err)
+	}
 
 	assertStrings(t, got, want)
 
 }
 
+
+
+
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q but want %q given key: %q ", got, want, "test")
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
+
+func assertErrror(t testing.TB, got, want error) {
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
